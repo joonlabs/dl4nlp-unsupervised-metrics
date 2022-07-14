2716efd648ce90aa2b747fe1b7384409462224b3
Benjamin Hirsch <bhirsch@techfak.de>
2022-07-14 08:35:01 +0200

    limit training datasets to 2007 only

--- metrics/utils/dataset.py
+++ metrics/utils/dataset.py
@@ -83,7 +83,8 @@ class DatasetLoader():
                     "http://data.statmt.org/cc-100"
                 ),
             },
-            "versions": list(range(2007, 2021)),
+            #"versions": list(range(2007, 2021)),
+            "versions": list(range(2007, 2008)),
             "samples": (40000, 4000000),
         }
     @property
